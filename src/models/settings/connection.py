7206e51216b85ebbf37afdc78c9249b26cf699a8
Walber <walber.araujo@getcandidly.com>
2024-04-02 11:51:37 -0300

    feat: implementing events repository with tests to insert and retrieve information

--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-class DBConnectionHandler:
+class __DBConnectionHandler:
   def __init__(self) -> None:
     self.__connection_string = "{}:///{}".format(
       "sqlite",
       "storage.db"
     )
     self.__engine = None
-    self.__session = None
+    self.session = None
   
   def connect_to_db(self) -> None:
     self.__engine = create_engine(self.__connection_string)
@@ -16,13 +16,12 @@ class DBConnectionHandler:
   def get_engine(self):
     return self.__engine
   
-  def get_session(self):
-    return self.__session
-  
   def __enter__(self):
     session_maker = sessionmaker()
-    self.__session = session_maker(bind=self.__engine)
+    self.session = session_maker(bind=self.__engine)
     return self
 
-  def __exit__(self):
-    self.__session.close()
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    self.session.close()
+
+db_connection_handler = __DBConnectionHandler()
