7544673dba14ced85e5e46d63f0ea0edb6c4952e
Walber <walber.araujo@getcandidly.com>
2024-04-05 13:52:17 -0300

    feat: Implementing route to get all attendees by event

--- src/data/atendees_handler.py
+++ src/data/atendees_handler.py
@@ -42,3 +42,25 @@ class AttendeesHandler:
       },
       status_code=200
     )
+  
+  def find_attendees_from_event(self, http_request: HttpRequest) -> HttpResponse:
+    event_id = http_request.param["event_id"]
+    attendees = self.__attendees_repository.get_attendees_by_event_id(event_id)
+    if not attendees: raise Exception("Participants not found!")
+
+    formatted_attendees = []
+    for attendee in attendees:
+      formatted_attendees.append(
+        {
+          "id": attendee.id,
+          "name": attendee.name,
+          "email": attendee.email,
+          "checked_in_at": attendee.checked_in_at,
+          "created_at": attendee.created_at
+        }
+      )
+    
+    return HttpResponse (
+      body={"attendees": formatted_attendees},
+      status_code=200
+    )
